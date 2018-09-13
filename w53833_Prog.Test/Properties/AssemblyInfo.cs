921719d78d3d0d7f788bf229cca8db983f8bc28b
MikeC94PL <41748684+MikeC94PL@users.noreply.github.com>
2018-09-13 19:13:13 +0200

    Projekt

---
+++ w53833_Prog.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("w53833_Prog.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("w53833_Prog.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("123d31f3-ca2d-45bd-b4ea-7b6445fee99e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ w53833_Prog.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using w53833_Prog;
+
+namespace w53833_Prog.Test
+{
+    
+    [TestClass()]
+    public class UnitTest1
+    {
+        
+        [TestMethod()]
+        public void TestMethod1()
+        {
+            var target = new 
+            Assert
+        }
+    }
+}
